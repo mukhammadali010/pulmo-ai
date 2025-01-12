689853220655e2eb406b0f9212568ffc687527d3
Muhammadali <test@Tests-MacBook-Pro.local>
2025-01-12 23:40:14 +0500

    pulmo-ai started

---
+++ src/app/app.routes.ts
@@ -0,0 +1,11 @@
+import { Routes } from '@angular/router';
+
+export const routes: Routes = [
+    {path:'home' , loadComponent:()=>import('./pages/components/home/home.component')},
+    {path:'login' , loadComponent:()=>import('./pages/components/login/login.component')},
+    {path:'logout' , loadComponent:()=>import('./pages/components/logout/logout.component')},
+    {path:'profile' , loadComponent:()=>import('./pages/components/profile/profile.component')},
+    {path:'not-found' , loadComponent:()=>import('./shared/components/not-found/not-found.component')},
+    {path:'' , redirectTo:'home' , pathMatch:'full'},
+    {path:'**' , redirectTo:'not-found'},
+];
