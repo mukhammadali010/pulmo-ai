93d981c5a5e06c2cb1bbedfb7d7c90f8078e8827
Muhammadali <test@Tests-MacBook-Pro.local>
2025-01-21 14:58:33 +0500

    dialog added

--- src/app/pages/components/home/home.component.ts
+++ src/app/pages/components/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { ToolbarOverviewExample } from "../../../shared/components/navbar/navbar.component";
 import { TabGroupAlignExample } from "../../../shared/components/tabs/tabs.component";
+import { DialogDataExample } from "../../../shared/components/dialog/dialog.component";
 
 @Component({
   selector: 'app-home',
-  imports: [ToolbarOverviewExample, TabGroupAlignExample],
+  imports: [ToolbarOverviewExample, TabGroupAlignExample, DialogDataExample],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
---
+++ src/app/shared/components/dialog/dialog.component.ts
@@ -0,0 +1,41 @@
+import {Component, inject} from '@angular/core';
+import {
+  MatDialog,
+  MAT_DIALOG_DATA,
+  MatDialogTitle,
+  MatDialogContent,
+} from '@angular/material/dialog';
+import {MatButtonModule} from '@angular/material/button';
+
+export interface DialogData {
+  animal: 'panda' | 'unicorn' | 'lion';
+}
+
+/**
+ * @title Injecting data when opening a dialog
+ */
+@Component({
+  selector: 'dialog-data-example',
+  templateUrl: './dialog-data-example.html',
+  imports: [MatButtonModule],
+})
+export class DialogDataExample {
+  dialog = inject(MatDialog);
+
+  openDialog() {
+    this.dialog.open(DialogDataExampleDialog, {
+      data: {
+        animal: 'panda',
+      },
+    });
+  }
+}
+
+@Component({
+  selector: 'dialog-data-example-dialog',
+  templateUrl: './dialog-data-example-dialog.html',
+  imports: [MatDialogTitle, MatDialogContent],
+})
+export class DialogDataExampleDialog {
+  data = inject(MAT_DIALOG_DATA);
+}
