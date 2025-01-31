012124593bb66f76843f12e51495cf2a4823061d
Muhammadali <test@Tests-MacBook-Pro.local>
2025-01-31 14:20:24 +0500

    chart added

--- src/app/shared/components/parametrlar/parametrlar.component.ts
+++ src/app/shared/components/parametrlar/parametrlar.component.ts
@@ -1,19 +1,19 @@
 import { Component, inject, Inject } from "@angular/core"
 import { ParametrSavollar, savol } from "../../models/frontend/parametr-savollar"
 import { MatRadioModule } from "@angular/material/radio"
-import { CommonModule, NgIf } from "@angular/common"
+import { CommonModule, NgFor, NgIf } from "@angular/common"
 import { FormsModule } from "@angular/forms"
 import { MatButtonModule } from "@angular/material/button"
 import { MatIconModule } from "@angular/material/icon"
 import { type ParametrAnswers, calculateResults } from "../../models/frontend/parametr-result-types"
 import { MatDialog, MAT_DIALOG_DATA, MatDialogModule, MatDialogRef } from "@angular/material/dialog"
-
+import { CanvasJSAngularChartsModule } from '@canvasjs/angular-charts';
 @Component({
   selector: "app-parametrlar",
   standalone: true,
   templateUrl: "./parametrlar.component.html",
   styleUrls: ["./parametrlar.component.scss"],
-  imports: [MatRadioModule, FormsModule, MatButtonModule, MatIconModule, NgIf, MatDialogModule, CommonModule],
+  imports: [MatRadioModule, FormsModule, MatButtonModule, MatIconModule, MatDialogModule, CommonModule],
 })
 export class Parametrlar {
   savollar = savol
@@ -76,13 +76,35 @@ export class Parametrlar {
   selector: "dialog-data-example-dialog",
   standalone: true,
   templateUrl: "./dialog-data-example-dialog.html",
-  imports: [CommonModule, MatDialogModule],
+  imports: [CommonModule, NgFor, MatDialogModule , CanvasJSAngularChartsModule],
 })
 export class DialogDataExampleDialog {
+  chartOptions: any;
+
   constructor(
     @Inject(MatDialogRef) public dialogRef: MatDialogRef<DialogDataExampleDialog>,
     @Inject(MAT_DIALOG_DATA) public data: { results: { [key: string]: number } }
-  ) {}
+  ) {
+    this.chartOptions = this.createChartOptions(data.results);
+  }
+
+  createChartOptions(results: { [key: string]: number }) {
+    return {
+      title: {
+        text: "Kasalliklar foiz ko'rsatkichlari",
+      },
+      animationEnabled: true,
+      data: [
+        {
+          type: "column",
+          dataPoints: Object.keys(results).map((key) => ({
+            label: key,
+            y: results[key],
+          })),
+        },
+      ],
+    };
+  }
 
   closeDialog() {
     this.dialogRef.close()
