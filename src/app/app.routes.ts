7ddb9c3911db0090867530bc3585423df7facd12
Muhammadali <test@Tests-MacBook-Pro.local>
2025-02-17 16:15:14 +0500

    route changed

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,8 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-    {path:'home' , loadComponent:()=>import('./pages/components/home/home.component')},
     {path:'patients-info' , loadComponent:()=>import('./shared/components/patients/patients.component')},
+    {path:'home' , loadComponent:()=>import('./pages/components/home/home.component')},
    
     {path:'login' , loadComponent:()=>import('./pages/components/login/login.component')},
     {path:'signup' , loadComponent:()=>import('./pages/components/signup/signup.component')},
